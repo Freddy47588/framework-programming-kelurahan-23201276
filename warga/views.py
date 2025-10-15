8b9d2efb4191640bd762a7b49903fe452dd80fdc
unknown <osdfredi241@gmail.com>
2025-10-15 22:29:00 +0700

    Modul 3: tambah WargaCreateView & PengaduanCreateView (reverse_lazy + success_url)

--- warga/views.py
+++ warga/views.py
@@ -1,13 +1,32 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 from .models import Warga, Pengaduan
+from .forms import WargaForm, PengaduanForm
+
 
 class PengaduanListView(ListView):
     model = Pengaduan
     template_name = 'warga/pengaduan_list.html'
 
+
 class WargaListView(ListView):
     model = Warga  # template default: warga/warga_list.html
-    
+
+
 class WargaDetailView(DetailView):
     model = Warga  # template default: warga/warga_detail.html
 
+
+
+class WargaCreateView(CreateView):
+    model = Warga
+    form_class = WargaForm
+    template_name = 'warga/warga_form.html'
+    success_url = reverse_lazy('warga-list')
+
+
+class PengaduanCreateView(CreateView):
+    model = Pengaduan
+    form_class = PengaduanForm
+    template_name = 'warga/pengaduan_form.html'
+    success_url = reverse_lazy('pengaduan-list')
